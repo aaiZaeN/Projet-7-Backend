a8a411b1f7b043fa3ce51435898bc3090240a9fa
aaizaen <max.concept89@gmail.com>
2020-09-29 15:46:06 +0200

    Mise en place du model groupopost dans groupopost.js

--- models/groupopost.js
+++ models/groupopost.js
@@ -3,31 +3,24 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class GroupoPost extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+ let Groupopost = sequelize.define('Groupopost', {
+   idUSERS: DataTypes.INTEGER,
+   title: DataTypes.STRING,
+   content: DataTypes.STRING,
+   attachment: DataTypes.STRING,
+   likes: DataTypes.INTEGER
+ }, {
+   classMethods: {
+     associate: function(models) {
+       // associations can be defined here
 
-      models.GroupoPost.belongsTo(models.User, {
-        foreignKey: {
-          allowNull: false
-        }
-      })
-    }
-  };
-  GroupoPost.init({
-    idUSERS: DataTypes.INTEGER,
-    title: DataTypes.STRING,
-    content: DataTypes.STRING,
-    attachment: DataTypes.STRING,
-    likes: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'GroupoPost',
-  });
-  return GroupoPost;
+       models.Groupopost.belongsTo(models.User, {
+         foreignKey: {
+           allowfull: false
+         }
+       })
+     }
+   }
+ });
+ return Groupopost;
 };
